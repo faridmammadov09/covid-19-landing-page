103deea303e40d40d6ed5214dfe81517a0136010
Farid Mammadov <fareedmammadov@gmail.com>
2020-05-28 04:41:21 +0400

    some fixes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const menuOverlay = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menu__item");
 
+const cards = document.querySelectorAll(".card");
+
 hamburgerMenu.addEventListener("click", () => {
   menuOverlay.classList.toggle("small");
 });
@@ -9,5 +11,15 @@ hamburgerMenu.addEventListener("click", () => {
 menuItems.forEach((item) => {
   item.addEventListener("click", () => {
     menuOverlay.classList.remove("small");
+  });
+});
+
+cards.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    cards.forEach((card) => {
+      card.classList.remove("card--active");
+    });
+
+    card.classList.add("card--active");
   });
 });
